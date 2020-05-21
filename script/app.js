0cfe5852d8be6badecd8431db297f309066a7193
BeulahPrinci <54794829+BeulahPrinci@users.noreply.github.com>
2020-05-21 21:54:59 +0530

    Update app.js

--- script/app.js
+++ script/app.js
@@ -10,3 +10,25 @@ const weather={};
 weather.temperature={
     unit:"celsius"
 }
+const KELVIN = 273;
+const key ="82005d27a116c2880c8f0fcb866998a0";
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition,showError);
+
+}else{
+    notificationElement.style.display="block"
+    notificationElement.innerHTML="<p>Browser doesn't Support Geolocation</p>";
+
+}
+ function setPosition(position){
+     let latitude = position.coords.latitude;
+     let longitude = position.coords.longitude;
+    getWeather(latitude,longitude);
+ }
+ function showError(error){
+     notificationElement.style.display="block";
+     notificationElement.innerHTML=`<p> ${error.message}</p>`;
+ }
+
+ //get weather 
+ 
