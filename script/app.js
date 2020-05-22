2f9264202c9c6e6ffb8fe4428883d9374684a528
BeulahPrinci <54794829+BeulahPrinci@users.noreply.github.com>
2020-05-22 20:58:41 +0530

    Update app.js

--- script/app.js
+++ script/app.js
@@ -32,7 +32,7 @@ if('geolocation' in navigator){
 
  //get weather 
   function getWeather(latitude,longitude){
-     let api =`://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+     let api =`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
      fetch(api)
      .then(function(response){
          let data=response.json();
